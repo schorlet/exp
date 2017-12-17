a8ed4db53c47789f9da92719043a04867143bbf2
stephanec <stephanec@washingmachine>
2017-12-17 15:19:13 +0100

    Add a logger

--- kube/main.go
+++ kube/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Print("Starting the service...")
 	http.HandleFunc("/home",
 		func(w http.ResponseWriter, _ *http.Request) {
 			fmt.Fprint(w, "Hello! Your request was processed.\n")
 		})
-	http.ListenAndServe(":8000", nil)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
