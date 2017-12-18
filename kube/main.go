89a90de57f18af11fb3bd40c4315935dea150caa
stephanec <stephanec@washingmachine>
2017-12-18 21:37:33 +0100

    Configuration

--- kube/main.go
+++ kube/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	log.Print("Starting the service...")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set.")
+	}
 	router := Router()
-	if err := http.ListenAndServe(":8000", router); err != nil {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatal(err)
 	}
 }
