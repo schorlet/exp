7a8cccbfa7f7bf596d4f116835f44424f680d548
stephanec <stephanec@washingmachine>
2018-02-05 23:11:47 +0100

    Add multiErr

---
+++ gtimer/error.go
@@ -0,0 +1,25 @@
+package gtimer
+
+import "fmt"
+
+type multiErr struct {
+	Errors []error
+}
+
+func (me multiErr) Error() string {
+	return fmt.Sprint(me.Errors)
+}
+
+func (me multiErr) orNil() error {
+	if len(me.Errors) == 0 {
+		return nil
+	}
+	return me
+}
+
+func (me *multiErr) append(err error) error {
+	if err != nil {
+		me.Errors = append(me.Errors, err)
+	}
+	return err
+}
--- gtimer/sql.go
+++ gtimer/sql.go
@@ -6,25 +6,36 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func withTx(db *sqlx.DB, fn func(sqlx.Ext) error) (err error) {
+func withTx(db *sqlx.DB, fn func(sqlx.Ext) error) (rerr error) {
+	var errs multiErr
+	defer func() {
+		rerr = errs.orNil()
+	}()
+
 	tx, err := db.Beginx()
 	if err != nil {
-		return err
+		return errs.append(err)
 	}
 
 	defer func() {
 		if p := recover(); p != nil {
-			err = fmt.Errorf("%v", p)
-			_ = tx.Rollback()
-		} else if err != nil {
-			_ = tx.Rollback()
+			errs.append(fmt.Errorf("%v", p))
+			if err := tx.Rollback(); err != nil {
+				errs.append(err)
+			}
+		} else if rerr != nil {
+			if err := tx.Rollback(); err != nil {
+				errs.append(err)
+			}
 		} else {
-			err = tx.Commit()
+			if err := tx.Commit(); err != nil {
+				errs.append(err)
+			}
 		}
 	}()
 
 	if err := fn(tx); err != nil {
-		return err
+		return errs.append(err)
 	}
 	return nil
 }
