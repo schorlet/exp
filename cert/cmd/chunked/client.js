b8b0c9e7da19abcbc9973c439a76120fd8e8b217
stephanec <stephanec@washingmachine>
2018-09-02 21:58:33 +0200

    Promise with timeout

--- cert/cmd/chunked/client.js
+++ cert/cmd/chunked/client.js
@@ -14,7 +14,42 @@ fetch('/')
 			console.log(data);
 			return pump();
 		})
-		.catch(err => console.log(err));
+		.catch(err => console.error(err));
 	};
 	return pump();
 });
+
+// -------------------
+
+var race = (promise, ms) => {
+	var timer = null;
+	return Promise.race([
+		new Promise((resolve, reject) => {
+			ms = ms || 900 + Math.random()*200;
+			timer = setTimeout(reject, ms, 'timeout:'+ms);
+		}),
+		promise.then((value) => {
+			clearTimeout(timer);
+			return value;
+		})
+	]);
+};
+
+race(fetch('/'), 2000)
+.then(response => response.body.getReader())
+.then(reader => {
+	var decoder = new TextDecoder();
+	var pump = () => {
+		return race(reader.read()).then(chunk => {
+			if (chunk.done) {
+				return;
+			}
+			var data = decoder.decode(chunk.value);
+			console.log(data);
+			return pump();
+		})
+		.catch(err => console.error(err));
+	};
+	return pump();
+})
+.catch(err => console.error(err));
